06434a7894c33db70c2659de1740dabea11232c8
eze1er <papaezechiel@gmail.com>
2021-12-01 20:21:58 +0000

    First commit for client.js

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require('net');
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log("It's got a sharp tongue, this server!");
+  });
+  
+  return conn;
+};
+
+module.exports = connect;
