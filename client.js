639ad9a6f7d32cbad961a96d51e6151976f4745a
eze1er <papaezechiel@gmail.com>
2021-12-01 21:58:04 +0000

    Commit fo move command

--- client.js
+++ client.js
@@ -7,10 +7,32 @@ const connect = function () {
   });
 
   // interpret incoming data as text
+  let timer = 1000;
   conn.setEncoding("utf8");
   conn.on("connect", () => {
     // code that does something when the connection is first established
     console.log("It's got a sharp tongue, this server!");
+    console.log("Successfully connected to game server" );
+    conn.write("Name: EZE");
+    for (let i = 0; i < timer; i++) {
+      timer = Math.random(0.9) * 3000;
+      // setInterval(() => {
+      conn.write(("move: up"),100);
+      conn.write("Name: EZE");
+      conn.write("move: down");
+      conn.write("Name: EZE");
+      conn.write("move: up");
+      conn.write("Name: EZE");
+      conn.write("move: up");
+      conn.write("Name: EZE");
+      conn.write("move: left");
+      conn.write("Name: EZE");
+      conn.write("move: down");
+      conn.write("Name: EZE");
+      conn.write("move: right");
+      conn.write("Name: EZE");
+    // }, timer);
+  }
   });
   
   return conn;
--- play.js
+++ play.js
@@ -2,5 +2,6 @@ const { Socket } = require("dgram");
 const net = require("net");
 const connect = require('./client');
 
+
 console.log("Connecting ...");
 connect();
